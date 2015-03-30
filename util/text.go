d06661cc2d1a14c815a1585e754e51df71c23f0a
shlomi-noach <shlomi@openark.org>
2015-03-30 16:09:43 +0200

    ParseSimpleTime changed to SimpleTimeToSeconds

--- util/text.go
+++ util/text.go
@@ -21,31 +21,30 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
-	"time"
 )
 
 // ParseSimpleTime parses input in the format 7s, 55m, 3h, 31d, 4w (second, minute, hour, day, week)
 // The time.ParseDuration() function should have done this, but it does not support "d" and "w" extensions.
-func ParseSimpleTime(simpleTime string) (time.Duration, error) {
+func SimpleTimeToSeconds(simpleTime string) (int, error) {
 	if matched, _ := regexp.MatchString("^[0-9]+s$", simpleTime); matched {
 		i, _ := strconv.Atoi(simpleTime[0 : len(simpleTime)-1])
-		return time.Duration(i) * time.Second, nil
+		return i, nil
 	}
 	if matched, _ := regexp.MatchString("^[0-9]+m$", simpleTime); matched {
 		i, _ := strconv.Atoi(simpleTime[0 : len(simpleTime)-1])
-		return time.Duration(i) * time.Minute, nil
+		return i * 60, nil
 	}
 	if matched, _ := regexp.MatchString("^[0-9]+h$", simpleTime); matched {
 		i, _ := strconv.Atoi(simpleTime[0 : len(simpleTime)-1])
-		return time.Duration(i) * time.Hour, nil
+		return i * 60 * 60, nil
 	}
 	if matched, _ := regexp.MatchString("^[0-9]+d$", simpleTime); matched {
 		i, _ := strconv.Atoi(simpleTime[0 : len(simpleTime)-1])
-		return time.Duration(i) * time.Hour * 24, nil
+		return i * 60 * 60 * 24, nil
 	}
 	if matched, _ := regexp.MatchString("^[0-9]+w$", simpleTime); matched {
 		i, _ := strconv.Atoi(simpleTime[0 : len(simpleTime)-1])
-		return time.Duration(i) * time.Hour * 24 * 7, nil
+		return i * 60 * 60 * 24 * 7, nil
 	}
 	return 0, errors.New(fmt.Sprintf("Cannot parse simple time: %s", simpleTime))
 }
